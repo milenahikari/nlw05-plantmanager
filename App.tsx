58db817e8c770794e18403d9436c559a4df8f410
Milena <milenahikari11@gmail.com>
2021-04-28 22:40:22 -0300

    Adicionando rotas na aplicacao

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import AppLoading from 'expo-app-loading';
-import { Confirmation } from './src/pages/Confirmation';
+
+import Routes from './src/routes';
+
 import {
   useFonts,
   Jost_400Regular,
@@ -18,6 +20,6 @@ export default function App() {
   }
 
   return (
-    <Confirmation />
+    <Routes />
   );
 }
