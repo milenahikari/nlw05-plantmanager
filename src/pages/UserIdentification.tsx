d126a0eaf9c49167261db7e14845ebacbb3829a2
Milena <milenahikari11@gmail.com>
2021-04-28 22:15:28 -0300

    Melhorando a tela de confirmacao da identificacao do usuario com dicas de UX

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   KeyboardAvoidingView,
   Platform,
@@ -15,6 +15,24 @@ import colors from '../../styles/colors';
 import fonts from '../../styles/fonts';
 
 export function UserIdentification() {
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+  const [name, setName] = useState<string>();
+
+  function handleInputBlur() {
+    setIsFocused(false);
+    setIsFilled(!!name);
+  }
+
+  function handleInputFocus() {
+    setIsFocused(true);
+  }
+
+  function handleInputChange(value: string) {
+    setIsFilled(!!value);
+    setName(value);
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <KeyboardAvoidingView
@@ -25,8 +43,8 @@ export function UserIdentification() {
           <View style={styles.form}>
             <View style={styles.header}>
               <Text style={styles.emoji}>
-                😄
-          </Text>
+                {isFilled ? '😄' : '😀'}
+              </Text>
 
               <Text style={styles.title}>
                 Como podemos {'\n'}
@@ -35,8 +53,14 @@ export function UserIdentification() {
             </View>
 
             <TextInput
-              style={styles.input}
+              style={[
+                styles.input,
+                (isFocused || isFilled) && { borderColor: colors.green }
+              ]}
               placeholder="Digite um nome"
+              onBlur={handleInputBlur}
+              onFocus={handleInputFocus}
+              onChangeText={handleInputChange}
             />
 
             <View style={styles.footer}>
