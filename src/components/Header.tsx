048bb24bf3019918489b1b2c41c0c8288ab1184c
Milena <milenahikari11@gmail.com>
2021-04-29 22:36:02 -0300

    Desenvolvendo o header da aplicacao

---
+++ src/components/Header.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+import userImg from '../assets/milena.jpg';
+import colors from '../../styles/colors';
+import fonts from '../../styles/fonts';
+
+export function Header() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.greeting}>Olá,</Text>
+        <Text style={styles.userName}>Milena</Text>
+      </View>
+
+      <Image source={userImg} style={styles.image} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 20,
+    marginTop: getStatusBarHeight(),
+    padding: 20
+  },
+  greeting: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.text
+  },
+  userName: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 40
+  },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 40
+  }
+});
--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { Button } from '../components/Button';
 
@@ -7,6 +8,12 @@ import colors from '../../styles/colors';
 import fonts from '../../styles/fonts';
 
 export function Confirmation() {
+  const navigation = useNavigation();
+
+  function handleMoveOn() {
+    navigation.navigate('PlantSelect');
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
@@ -25,6 +32,7 @@ export function Confirmation() {
         <View style={styles.footer}>
           <Button
             title="Começar"
+            onPress={handleMoveOn}
           />
         </View>
       </View>
